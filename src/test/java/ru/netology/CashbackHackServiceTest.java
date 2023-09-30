64079645ca235b230f0e848b244c751f63053b94
Karina <not-funny@mail.ru>
2023-09-30 13:03:57 +0700

    junit4

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,17 +1,18 @@
 package ru.netology;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
-    @org.testng.annotations.Test
-    public void testRemain(){
+    @org.junit.Test
+    public void testRemain() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
-        assertEquals (actual, expected);
+        assertEquals(actual, expected);
     }
-    @org.testng.annotations.Test
+
+    @org.junit.Test
     public void testRemainOver1000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1200;
@@ -19,7 +20,8 @@ public class CashbackHackServiceTest {
         int expected = 0;
         assertEquals(actual, expected);
     }
-    @org.testng.annotations.Test
+
+    @org.junit.Test
     public void testRemainWithCashback() {
         CashbackHackService service = new CashbackHackService();
         int amount = 700;
@@ -28,7 +30,8 @@ public class CashbackHackServiceTest {
         assertEquals(actual, expected);
 
     }
-    @org.testng.annotations.Test
+
+    @org.junit.Test
     public void testRemainZero() {
         CashbackHackService service = new CashbackHackService();
         int amount = 0;
